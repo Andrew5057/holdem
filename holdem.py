270f1ed124f28fee069722bf8dfbaa2f4bc187d4
A-Miner1000 <84680088+Andrew5057@users.noreply.github.com>
2023-01-02 13:27:57 -0600

    Update holdem.py

--- holdem.py
+++ holdem.py
@@ -1,22 +1,45 @@
 class Card:
-    def __init__(self, suit: str, rank: str):
-        if suit in ('heart', 'diamond', 'spade', 'club'):
-            self.suit: str = suit
-        self.rank: str = str(rank)
-        if self.rank.isnumeric():
-            self.value: int = int(rank)
-        else:
-            match self.rank:
-                case 'jack':
-                    self.value: int = 11
-                case 'queen':
-                    self.value: int = 12
-                case 'king':
-                    self.value: int = 13
-                case 'ace':
-                    self.value: int = 14
-x = Card('heart', 'ace')
+    def __init__(self, suit: str, rank: str = None):
+        if rank is None:
+            suit_initial: str = suit[-1]
+            rank: str = suit[:-1]
+            match suit_initial:
+                case 'H':
+                    self.suit: str = 'Heart'
+                case 'S':
+                    self.suit: str = 'Suit'
+                case 'D':
+                    self.suit: str = 'Diamond'
+                case 'C':
+                    self.suit: str = 'Club'
+            self.rank = rank
+            if self.rank.isnumeric():
+                self.value = int(self.rank)
+            else:
+                match self.rank:
+                    case 'Jack':
+                        self.value: int = 11
+                    case 'Queen':
+                        self.value: int = 12
+                    case 'King':
+                        self.value: int = 13
+                    case 'Ace':
+                        self.value: int = 14
 
-print(x.suit)
-print(x.rank)
-print(x.value)
+        else:
+            self.suit: str = suit
+            self.rank: str = str(rank)
+            if self.rank.isnumeric():
+                self.value: int = int(rank)
+            else:
+                match self.rank:
+                    case 'Jack':
+                        self.value: int = 11
+                    case 'Queen':
+                        self.value: int = 12
+                    case 'King':
+                        self.value: int = 13
+                    case 'Ace':
+                        self.value: int = 14
+x = Card('4H')
+y = Card('4', 'King')
