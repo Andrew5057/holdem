617245c6dc997abd99c65d2207b8d14ab77b7b2f
A-Miner1000 <84680088+Andrew5057@users.noreply.github.com>
2023-01-02 14:36:09 -0600

    Several changes
     - Added tests for cards of rank 10
     - Added Ace-1 substitution
     - Resolved bug arising from "rank" vs "self.rank" discrepancy
     - Added some type hints (decorative)

--- holdem.py
+++ holdem.py
@@ -2,16 +2,16 @@ class Card:
     def __init__(self, rank: str, suit: str = None):
         if suit is None:
             # Consider a check here for a two character rank
-            suit = rank[-1]
-            rank = rank[:-1]
-            
+            suit: str = rank[-1]
+            rank: str = rank[:-1]
+        
         self.suit: str = suit[0].upper()
         
-        if self.rank.isnumeric():
+        if rank.isnumeric():
             self.rank: str = rank
             self.value: int = int(self.rank)
         else:
-            self.rank = rank[0].upper()
+            self.rank: str = rank[0].upper()
             match self.rank:
                 case 'J':
                     self.value: int = 11
@@ -21,12 +21,21 @@ class Card:
                     self.value: int = 13
                 case 'A':
                     self.value: int = 14
+        
+        if self.rank == 1:
+            self.rank: str = 'A'
+        if self.value == 1:
+            self.value: int = 14
                     
 x = Card('4H')
 print(f'{x.suit}, {x.rank}, {x.value}')
 x = Card('KH')
 print(f'{x.suit}, {x.rank}, {x.value}')
+x = Card('10H')
+print(f'{x.suit}, {x.rank}, {x.value}')
 x = Card('4', 'Heart')
 print(f'{x.suit}, {x.rank}, {x.value}')
 x = Card('King', 'Heart')
+print(f'{x.suit}, {x.rank}, {x.value}')
+x = Card('10', 'Heart')
 print(f'{x.suit}, {x.rank}, {x.value}')
