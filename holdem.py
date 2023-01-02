baee8fce6abc7c1063da7e0b14bf18cbcc52ce54
A-Miner1000 <84680088+Andrew5057@users.noreply.github.com>
2023-01-02 13:05:28 -0600

    Create holdem.py

---
+++ holdem.py
@@ -0,0 +1,18 @@
+class Card:
+    def __init__(self, suit: str, rank: str):
+        if suit in ('heart', 'diamond', 'spade', 'club'):
+            self.suit: str = suit
+        self.rank: str = str(rank)
+        if self.rank.isnumeric():
+            self.value: int = int(rank)
+        else:
+            match self.rank:
+                case 'jack':
+                    self.value: int = 11
+                case 'queen':
+                    self.value: int = 12
+                case 'king':
+                    self.value: int = 13
+                case 'ace':
+                    self.value: int = 14
+Card('heart', 'ace')
