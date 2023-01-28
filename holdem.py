e1a8d2ac7fa5d41b5770e76814c313495e2b7600
A-Miner1000 <84680088+Andrew5057@users.noreply.github.com>
2023-01-28 11:33:50 -0600

    Implement drawing
     - Game.next() draws for the next phase of the game, re-initializing once the game is over
     - Fixed a bug with the Player class

--- holdem.py
+++ holdem.py
@@ -100,7 +100,10 @@ class Deck:
 
 class Player:
     def __init__(self, *args):
-        self.hand: tuple = tuple([card for card in args])
+        if len(args) == 1:
+            self.hand = args[0]
+        else:
+            self.hand: tuple[Card] = tuple([card for card in args])
 
 class Game:
     def __init__(self, player_count: int):
@@ -108,4 +111,18 @@ class Game:
         self.deck.shuffle()
         self.player_count: int = player_count
         self.players: list[Player] = [Player(self.deck.draw(2)) for player in range(player_count)]
-
+        # 0 = post-deal, 1 = post-flop, 2 = post-turn, 3 = post-river
+        self.state: int = 0
+        self.community_cards: list[Card] = []
+    
+    def next(self):
+        self.state += 1
+        match self.state:
+            case 1:
+                self.community_cards.extend(self.deck.draw(3))
+            case 2:
+                self.community_cards.extend(self.deck.draw(1))
+            case 3:
+                self.community_cards.extend(self.deck.draw(1))
+            case 4:
+                self.__init__(self.player_count)
