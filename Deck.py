6cef78fc2f9bfe5aece9f616dad60beea784ecae
A-Miner1000 <84680088+Andrew5057@users.noreply.github.com>
2023-01-29 12:59:58 -0600

    Fixed imports

---
+++ Deck.py
@@ -0,0 +1,18 @@
+from random import shuffle
+from .Card.py import *
+
+class Deck:
+    def __init__(self):
+        self.cards: list[Card] = [Card(str(rank), suit) for rank in range(1, 14) for suit in ('C', 'S', 'D', 'H')]
+
+    def shuffle(self):
+        shuffle(self.cards)
+
+    def draw(self, count=1) -> tuple[Card]:
+        return tuple([self.cards.pop() for i in range(count)])
+    
+    def __len__(self) -> int:
+        return len(self.cards)
+    
+    def __iter__(self):
+        yield from self.cards
