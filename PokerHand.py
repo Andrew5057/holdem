552c77528fad6772f755720ad02a355cf7b3de93
sy20082010 <57463812+sy20082010@users.noreply.github.com>
2023-01-29 12:37:31 -0600

    Create PokerHand.py

---
+++ PokerHand.py
@@ -0,0 +1,15 @@
+from holdemp.py import *
+
+class PokerHand:
+    def __init__(self, cards: list[Card]):
+        # Cards as they get introduced, no order
+        self.cards = cards
+        # String representations of hand
+        self.all_cards = "" # assign this later
+        self.diamonds = ""
+        self.hearts = ""
+        self.spades = ""
+        self.clubs = ""
+        
+
+
