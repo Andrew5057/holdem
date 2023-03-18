2b121637183250c17341f5217380e9be01abe2d6
A-Miner1000 <84680088+Andrew5057@users.noreply.github.com>
2023-03-18 13:38:44 -0500

    Reversed sort
    Saves time by running sort with reverse=True instead of sorting, then reversing

--- PokerHand.py
+++ PokerHand.py
@@ -6,7 +6,8 @@ class PokerHand:
     def __init__(self, cards: list[Card]):
         # Cards as they get introduced, no order
         self.cards: list[Card] = cards
-        self.cards.sort()
+        self.cards.sort(reverse=True)
+        
         # String representations of hand
         self.card_string: str = ""
         self.value_map: dict[str: int] = {'1': 10, '2': 2, '3': 3, '4':4, 
@@ -15,18 +16,16 @@ class PokerHand:
                                          'A': 14}
     
     def create_string(self):
-        self.cards.sort()
+        self.cards.sort(reverse=True)
         self.cards_string = ''.join([card.str for card in self.cards])
     def two_pair(self) -> tuple[int]:
-        reversed_string: str = self.cards_string[::-1]
-
         two_pair: re.Match = re.search(r'([1-9JQKA]).\1.*([1-9JQKA]).\2', 
-                                       reversed_string)
+                                       self.cards_string)
         if two_pair is not None:
             highest: int = self.value_map[two_pair.group(1)]
             second: int = self.value_map[two_pair.group(2)]
 
-            for char in reversed_string:
+            for char in self.cards_string:
                 if char in self.value_map and (not char == two_pair.group(1) and 
                                                not char == two_pair.group(2)):
                     kicker: int = self.value_map[char]
