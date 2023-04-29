436dda1e69e0ebf8076a984805456d713410b4ee
Andrew5057 <84680088+Andrew5057@users.noreply.github.com>
2023-04-29 15:47:38 -0500

    Revert "Everything broke again"
    This reverts commit e4a06e4c53f042ed059315c7fe1dfa80f07ca285.

--- PokerHand.py
+++ PokerHand.py
@@ -3,10 +3,10 @@ from Card import *
 import re
 
 class PokerHand:
-    value_map: dict[str: int] = {'A': 'E', 'K': 'D', 'Q': 'C', 'J': 'B',
-                                        'T': 'A', '9': '9', '8': '8', '7': '7',
-                                        '6': '6', '5': '5', '4': '4', '3': '3',
-                                        '2': '2'}
+    value_map: dict[str: int] = {'A': 14, 'K': 13, 'Q': 12, 'J': 11,
+                                        'T': 10, '9': 9, '8': 7, '7': 7,
+                                        '6': 6, '5': 5, '4': 4, '3': 3,
+                                        '2': 2}
     def __init__(self, cards: list[Card]):
         # Cards as they get introduced, no order
         self.cards: list[Card] = cards
@@ -15,16 +15,20 @@ class PokerHand:
     
     def create_string(self) -> None:
         self.cards.sort(reverse=True)
-        self.cards_string = ''.join([card.str for card in self.cards])[::2]
+        self.cards_string = ''.join([card.str for card in self.cards])
 
-        self.spades: tuple[int] = tuple([self.value_map[card.rank] for card in 
-                                         self.cards if card.suit == 'S'])
-        self.clubs: tuple[int] = tuple([self.value_map[card.rank] for card in 
-                                         self.cards if card.suit == 'C'])
-        self.diamonds: tuple[int] = tuple([self.value_map[card.rank] for card in 
-                                         self.cards if card.suit == 'D'])
-        self.hearts: tuple[int] = tuple([self.value_map[card.rank] for card in 
-                                         self.cards if card.suit == 'H'])
+        self.spades: tuple[int] = tuple([self.value_map[self.cards_string[i]] for i
+                                    in range(len(self.cards_string)-1) if
+                                    self.cards_string[i+1] == 'S'])
+        self.clubs: tuple[int] = tuple([self.value_map[self.cards_string[i]] for i
+                                    in range(len(self.cards_string)-1) if
+                                    self.cards_string[i+1] == 'C'])
+        self.diamonds: tuple[int] = tuple([self.value_map[self.cards_string[i]] for i
+                                    in range(len(self.cards_string)-1) if
+                                    self.cards_string[i+1] == 'D'])
+        self.hearts: tuple[int] = tuple([self.value_map[self.cards_string[i]] for i
+                                    in range(len(self.cards_string)-1) if
+                                    self.cards_string[i+1] == 'H'])
     
     def straight_flush(self) -> int:
         count = 0
@@ -80,18 +84,13 @@ class PokerHand:
         return 5 if (count == 4) and (14 in self.spades) else None
     
     def four_of_a_kind(self) -> int:
-        four_of_a_kind: re.Match = re.search(r'([2-9TJQKA])\1\1\1', self.cards_string)
+        four_of_a_kind: re.Match = re.search(r'([2-9TJQKA]).\1.\1.\1', self.cards_string)
         return self.value_map[four_of_a_kind.group(1)] if four_of_a_kind is not None else None
 
     def flush(self) -> tuple[int]:
-        if len(self.spades) >= 5:
-            return int(''.join(self.spades[:5]))
-        elif len(self.clubs) >= 5:
-            return int(''.join(self.clubs[:5]))
-        elif len(self.diamonds) >= 5:
-            return int(''.join(self.diamonds[:5]))
-        elif len(self.hearts) >= 5:
-            return int('5'+''.join(self.hearts[:5]), 16)
+        flush: re.Match = re.search(r'([2-9TJQKA])([HDSC]).*?([2-9TJQKA])\2.*?([2-9TJQKA])\2.*?([2-9TJQKA])\2.*?([2-9TJQKA])\2',
+                                    self.cards_string)
+        return tuple([self.value_map[rank] for rank in flush.groups() if rank not in 'HDSC']) if flush is not None else None
     
     
     def straight(self) -> int:
@@ -102,7 +101,7 @@ class PokerHand:
             if rank in self.cards_string:
                 if high is None: high = rank
                 count += 1
-                if count == 5: return int('4'+''.join([self.value_map[high]], 16))
+                if count == 5: return self.value_map[high]
             else:
                 count = 0
                 high = None
@@ -111,7 +110,7 @@ class PokerHand:
         return 5 if (count == 4) and ('A' in self.cards_string) else None
 
     def two_pair(self) -> tuple[int]:
-        two_pair: re.Match = re.search(r'([2-9TJQKA])\1.*?([2-9TJQKA])\2', 
+        two_pair: re.Match = re.search(r'([2-9TJQKA]).\1.*([2-9TJQKA]).\2', 
                                        self.cards_string)
         if two_pair is not None:
             highest: int = self.value_map[two_pair.group(1)]
