9aa886d75ec2c8bbffbf30580b9435f4942a0362
Andrew5057 <84680088+Andrew5057@users.noreply.github.com>
2023-07-01 14:23:03 -0500

    Create prototype ProbabilityCalculator.estimate()
    ProbabilityCalculator.estimate(opponents, n)  returns an estimated probability that at a table with the given number of opponents (default 8), at least one has a hand better than the player. n (default 1000) gives the number of samples to take.

--- Probabilities.py
+++ Probabilities.py
@@ -2,16 +2,16 @@ from Card import Card
 from Deck import Deck
 from PokerHand import PokerHand
 from itertools import combinations
-
+from random import choice
 
 class ProbabilityCalculator:
 
-    def __init__(self, cards: list[Card], n_players: int):
+    def __init__(self, player_hand: list[Card]):
         
-        self.player: PokerHand = PokerHand(cards)
+        self.player: str = f'{str(player_hand[0])}{str(player_hand[1])}'
         self.deck: Deck = Deck()
-        self.deck.remove(cards[0])
-        self.deck.remove(cards[1])
+        self.deck.remove(player_hand[0])
+        self.deck.remove(player_hand[1])
 
         possible_cards: list[str] = ['2H', '2D', '2S', '2C', '3H', '3D', 
                                      '3S', '3C', '4H', '4D', '4S', '4C',
@@ -22,18 +22,18 @@ class ProbabilityCalculator:
                                      'JH', 'JD', 'JS', 'JC', 'QH', 'QD', 
                                      'QS', 'QC', 'KH', 'KD', 'KS', 'KC', 
                                      'AH', 'AD', 'AS', 'AC']
-        possible_cards.remove(str(cards[0]))
-        possible_cards.remove(str(cards[1]))
+        possible_cards.remove(str(player_hand[0]))
+        possible_cards.remove(str(player_hand[1]))
         self.hands: dict[str: int] = {}
-        possible_hands = combinations(self.possible_cards, 2)
+        possible_hands: tuple = combinations(self.possible_cards, 2)
         for hand in possible_hands:
             c1, c2 = hand[0], hand[1]
             self.hands[f'{c1}{c2}'] = PokerHand(Card(c1),
                                                 Card(c2)).best_hand()
 
-        self.community_cards = []
+        self.community_cards: list[Card] = []
         
-    def next(self, *new_cards: Card) -> None:
+    def community(self, *new_cards: Card) -> None:
         '''Updates the ProbabilityCalculator to include new community_cards.
             Removes newly impossible hands accordingly. Also updates best_hand
             values for each hand in the hands dictionary.
@@ -56,4 +56,30 @@ class ProbabilityCalculator:
 
             full_hand = PokerHand(self.community_cards + [c1, c2])
             self.hands[hand] = full_hand.best_hand()
-    
+    def estimate(self, opponents:int=8, n:int=1000) -> float:
+        '''Estimates the probability that at least one person has a hand
+            better than the player's.
+        
+        Arguments:
+        opponents (int): The number of other players in play. Defaults to 8.
+        n (int): The number of random samples used for estimation  
+
+        Output: float in [0, 1] representing the estimated probability that
+        at least one opponent has a hand better than the player's.
+        '''
+
+        possible_hands: list = self.hands.keys()
+        sample_maxes: list[int] = []
+        for game_sample in range(n):
+            game_strengths: list[int] = []
+            compatible_hands: list[str] = list(possible_hands)
+            for opponent in range(opponents):
+                new_hand = choice(compatible_hands)
+                compatible_hands: list[str] = [hand for hand in compatible_hands
+                                               if (new_hand[:2] not in hand) and
+                                               (new_hand[2:] not in hand)]
+                game_strengths.append(self.hands[new_hand])
+            sample_maxes.append(max(game_strengths))
+        num_better = len([game for game in sample_maxes if game > self.hands[self.player]])
+        num_games = len(sample_maxes)
+        return num_better/num_games
