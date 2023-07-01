6e4348752c0e08370de78b2f01b460051efef49d
Andrew5057 <84680088+Andrew5057@users.noreply.github.com>
2023-07-01 15:21:30 -0500

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-from Card import Card
-from Probabilities import ProbabilityCalculator
-
-calculator = ProbabilityCalculator(Card('AS'), Card('AH'))
-
-calculator.add_community(Card('7D'), Card('5H'), Card('9H'))
-
-print(calculator.estimate())
