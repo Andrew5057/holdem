2563507c33c14398eb547127e7b452df937e89e2
Andrew5057 <84680088+Andrew5057@users.noreply.github.com>
2023-07-01 16:18:35 -0500

    Error handling, documentation

--- Probabilities.py
+++ Probabilities.py
@@ -7,7 +7,35 @@ from random import choice
 class ProbabilityCalculator:
 
     def __init__(self, card1: Card, card2: Card):
+        '''Defines a class that calculates the probability that at least one
+            player at a Texas Holdem table beats a given hand.
         
+        Positional arguments:
+        card1 (Card): One card in the player's hand.
+        card2 (Card): Another card in the player's hand.
+        
+        Instance variables:
+        player (PokerHand): A continuously updated PokerHand object
+            representing the player's hand, including community cards.
+        hands (dict[str: int]): A continuously updated dictionary
+            representing all possible hands. Keys are strings representing 
+            each hand's hole cards, such as 'KH5S' for a King of Hearts and a 
+            5 of Spades. Values are ints representing the hand's strength 
+            (including community cards), as defined by the PokerHand class.
+        community_cards: A continuously updated list representing community
+            cards.
+
+        Methods:
+        add_community: Adds cards to the instance's community cards.
+        estimate: Uses random sampling to estimate the probability of at 
+            least one player at the table beating the player's hand.
+
+        '''
+        if not isinstance(card1, Card):
+            raise TypeError('Positional variable card1 must be of type Card.')
+        if not isinstance(card2, Card):
+            raise TypeError('Positional variable card2 must be of type Card.')
+
         self.player: PokerHand = PokerHand([card1, card2])
 
         possible_cards: list[str] = ['2H', '2D', '2S', '2C', '3H', '3D', 
@@ -41,6 +69,17 @@ class ProbabilityCalculator:
         
         Output: None
         '''
+        for card in new_cards:
+            if not isinstance(card, Card):
+                raise TypeError('All arguments must be of type Card.')
+            if card in self.community_cards:
+                raise ValueError('No arguments can already exist in \
+                                 community_cards.')
+
+        if len(set(new_cards)) != len(new_cards):
+            # Catchces if duplicate cards were entered as arguments.
+            raise ValueError('All arguments must be unique.')
+
         self.community_cards.extend(new_cards)
         hand_list = set(self.hands.keys())
         for card in new_cards:
@@ -54,6 +93,7 @@ class ProbabilityCalculator:
                 continue
             full_hand = PokerHand(self.community_cards + [c1, c2])
             self.hands[hand] = full_hand.best_hand()
+    
     def estimate(self, opponents:int=8, n:int=10000) -> float:
         '''Estimates the probability that at least one person has a hand
             better than the player's.
@@ -66,6 +106,10 @@ class ProbabilityCalculator:
         Output: float in [0, 1] representing the estimated probability that
         at least one opponent has a hand better than the player's.
         '''
+        if not isinstance(opponents, int):
+            raise TypeError('Default argument opponents must be of type int.')
+        if not isinstance(n, int):
+            raise TypeError('Default argument n must be of type int.')
 
         possible_hands: list = self.hands.keys()
         sample_maxes: list[int] = []
