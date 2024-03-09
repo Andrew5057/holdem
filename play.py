17f60248779439d074a440c7c0b5fe3955c69630
sy20082010 <57463812+sy20082010@users.noreply.github.com>
2024-03-09 14:58:23 -0600

    working version

--- play.py
+++ play.py
@@ -5,23 +5,25 @@ from Table import Table
 import os
 import sys
 
+opponents = [6, 4, 2]
+
 table = Table()
 
 # New game
 table.new_game()
-table.analyze_and_display()
+table.analyze_and_display(opponents)
 
 # Flop
 if input("Hit Enter for the flop or 'x' to exit")=='x': sys.exit()
 table.draw_community(num_cards=3)
-table.analyze_and_display()
+table.analyze_and_display(opponents)
 
 # Turn
 input("Any key + Enter for the turn card")
 table.draw_community(num_cards=1)
-table.analyze_and_display()
+table.analyze_and_display(opponents)
 
 # River
 input("Any key + Enter for the river card")
 table.draw_community(num_cards=1)
-table.analyze_and_display()
+table.analyze_and_display(opponents)
